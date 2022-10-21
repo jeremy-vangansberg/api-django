9736c8a37cec519ceeaeacb62a5d2138cd559a91
jeremy Vangansberg <jeremy.vangansberg@gmail.com>
2022-10-21 14:44:51 +0200

    auth rdy

--- divers/urls.py
+++ divers/urls.py
@@ -3,8 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home_page),
-    path('contact/', views.contact_page),
-    path('about/', views.about_page),
-    path('signup/', views.SignupPage.as_view()),
+    path('', views.home_page, name='home'),
+    path('contact/', views.contact_page, name='contact'),
+    path('about/', views.about_page, name='about'),
+    path('signup/', views.SignupPage.as_view(), name='signup'),
 ]
