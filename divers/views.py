6433e0b76982dd0b3a39b14eb4e6e66e73b8a100
jeremy Vangansberg <jeremy.vangansberg@gmail.com>
2022-10-28 09:12:41 +0200

    post demo

--- divers/views.py
+++ divers/views.py
@@ -3,6 +3,7 @@ from django.contrib.auth.decorators import login_required
 from django.views.generic import CreateView
 from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
+from . import forms
 
 def home_page(request):
     title = "Crypto Live"
@@ -20,6 +21,6 @@ def special_view(request):
     return render(request, 'divers/special_view_page.html')
 
 class SignupPage(CreateView):
-    form_class = UserCreationForm
+    form_class = forms.UserCreateForm
     success_url = reverse_lazy('login')
     template_name = 'registration/signup.html'
